649c37427d6921c67410e116972692f7c2f58472
charlest001 <charlest001@github.com>
2019-12-30 12:46:46 +0200

    First commit

---
+++ src/system.cpp
@@ -0,0 +1,39 @@
+#include <unistd.h>
+#include <cstddef>
+#include <set>
+#include <string>
+#include <vector>
+
+#include "process.h"
+#include "processor.h"
+#include "system.h"
+#include "linux_parser.h"
+
+using std::set;
+using std::size_t;
+using std::string;
+using std::vector;
+
+// TODO: Return the system's CPU
+Processor& System::Cpu() { return cpu_; }
+
+// TODO: Return a container composed of the system's processes
+vector<Process>& System::Processes() { return processes_; }
+
+
+std::string System::Kernel() { return LinuxParser::Kernel(); }
+
+// TODO: Return the system's memory utilization
+float System::MemoryUtilization() { return 0.0; }
+
+
+std::string System::OperatingSystem() { return LinuxParser::OperatingSystem(); }
+
+// TODO: Return the number of processes actively running on the system
+int System::RunningProcesses() { return 0; }
+
+
+int System::TotalProcesses() { return LinuxParser::TotalProcesses(); }
+
+// TODO: Return the number of seconds since the system started running
+long int System::UpTime() { return LinuxParser::UpTime(); }
